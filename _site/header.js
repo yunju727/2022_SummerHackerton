3f97903fc7f44c727e2be9b449efa9a35579559a
CE-sw <2seowon@naver.com>
2022-07-08 05:17:31 +0900

    .

---
+++ _site/header.js
@@ -0,0 +1,10 @@
+var header = `
+<p><a href="index.html">Travel</a><p>
+<ul>
+    <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-pen-to-square"></i></a></button>
+    <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-heart"></i></a></button>
+    <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-user"></i></a></button>
+</ul>
+`;
+
+document.write(header)
