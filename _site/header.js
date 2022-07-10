8d7911a4af7167132c4908253f079afeb1628277
CE-sw <2seowon@naver.com>
2022-07-10 22:02:22 +0900

    .

--- _site/header.js
+++ _site/header.js
@@ -1,9 +1,14 @@
 var header = `
-<p><a href="index.html">Travel</a><p>
 <ul>
-    <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-pen-to-square"></i></a></button>
-    <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-heart"></i></a></button>
-    <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-user"></i></a></button>
+    <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-bars-staggered"></i></a></button></li>
+    <li><a href="index.html">Home</a></li>
+    <li><button class = "menu" ><a href="#">Contact</a></button></li>
+    <li><button class = "menu" ><a href="#">Bug report</a></button></li>
+</ul>
+<ul>
+    <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-pen-to-square"></i></a></button></li>
+    <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-heart"></i></a></button></li>
+    <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-user"></i></a></button></li>
 </ul>
 `;
 
