143964b08238ba3326e2ec3b3abefad5477dea7d
CE-sw <2seowon@naver.com>
2022-07-10 22:41:18 +0900

    .

--- _site/header.js
+++ _site/header.js
@@ -1,12 +1,12 @@
 var header = `
-<ul>
-    <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-bars-staggered"></i></a></button></li>
-    <li><a href="index.html">Home</a></li>
-    <li><button class = "menu" ><a href="#">Contact</a></button></li>
-    <li><button class = "menu" ><a href="#">Bug report</a></button></li>
+<ul id = "left">
+    <li><button class = " menu" ><a href="#">&#9776;</a></button></li>
+    <li><button class = "menu" ><a href="index.html">Home</a></button></li>
+    <li><button class = "menu del" ><a href="#">Contact</a></button></li>
+    <li><button class = "menu del" ><a href="#">Bug report</a></button></li>
 </ul>
-<ul>
-    <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-pen-to-square"></i></a></button></li>
+<ul id = "right">
+    <li><button class = "hvr-float-shadow menu del" ><a href="#"><i class="fa-regular fa-pen-to-square"></i></a></button></li>
     <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-heart"></i></a></button></li>
     <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-user"></i></a></button></li>
 </ul>
