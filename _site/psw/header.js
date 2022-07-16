ebf7207a081c7a63542db5cbc1a48592f0dead15
CE-sw <2seowon@naver.com>
2022-07-16 16:09:40 +0900

    .

--- _site/psw/header.js
+++ _site/psw/header.js
@@ -5,7 +5,7 @@ var header = `
     <li><button class = "menu del" ><a href="#">Bug report</a></button></li>
 </ul>
 <ul id = "right">
-    <li><button class = "hvr-float-shadow menu del" ><a href="#"><i class="fa-regular fa-pen-to-square"></i></a></button></li>
+    <li><button class = "hvr-float-shadow menu del" ><a href="./sgh/write1.html"><i class="fa-regular fa-pen-to-square"></i></a></button></li>
     <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-heart"></i></a></button></li>
     <li><button class = "hvr-float-shadow menu" ><a href="user.html"><i class="fa-regular fa-user"></i></a></button></li>
 </ul>
