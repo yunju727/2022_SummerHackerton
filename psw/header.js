8236d11a3ce76ac091112d7ddca7984d069ed866
CE-sw <2seowon@naver.com>
2022-07-22 19:55:47 +0900

    .

--- psw/header.js
+++ psw/header.js
@@ -1,21 +1,26 @@
 var header = `
 <ul class = "mainheader" id = "left">
-    <li><button class = " menu" ><a href="#" onclick="openusermenu()">&#9776;</a></button></li>
+    <li><button class = " menu" id="ham"><a href="#">&#9776;</a></button></li>
     <li><button class = "menu" ><a href="../index.html">Home</a></button></li>
     <li><button class = "menu del" ><a href="../psw/bug report.html">Bug report</a></button></li>
 </ul>
 <ul class = "mainheader" id = "right">
     <li><button class = "hvr-float-shadow menu del" ><a href="../sgh/write1.html"><i class="fa-regular fa-pen-to-square"></i></a></button></li>
     <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-heart"></i></a></button></li>
-    <li><button class = "hvr-float-shadow menu" ><a href="#" onclick="openusermenu()"><i class="fa-regular fa-user"></i></a></button></li>
+    <li><button class = "hvr-float-shadow menu" id="us"><a href="#"><i class="fa-regular fa-user"></i></a></button></li>
 </ul>
 `;
 
 document.write(header)
 
-function openusermenu(){
-    document.getElementById('usermenu').style.height='3rem';
-}
-function closeusermenu(){
-    document.getElementById('usermenu').style.height='0';
-}
+let user = document.querySelector("#us");
+let ham = document.querySelector("#ham");
+
+user.addEventListener('click', () => {
+    const usermenu = document.querySelector('.usermenu');
+    usermenu.classList.toggle('active')
+})
+
+ham.addEventListener('click', () => {
+    document.getElementById('sidemenu').style.width='250px';
+})
