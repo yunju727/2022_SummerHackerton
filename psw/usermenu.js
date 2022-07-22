f624f3c5b612883361fdcec5f101a5f40d4be9e7
CE-sw <2seowon@naver.com>
2022-07-22 20:03:49 +0900

    .

---
+++ psw/usermenu.js
@@ -0,0 +1,10 @@
+var usermenu = `
+<div class="usermenu">
+<li id="guide">Please Login</li>
+<ul id="usermenu">
+    <li><a href="../psw/user.html" id="prof">Profile</a></li>
+    <li><a href="#">Sign in</a></li>
+</ul>
+</div>`
+
+document.write(usermenu)
