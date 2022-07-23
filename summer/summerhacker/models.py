53fd8fa659f5078772cd466966bb0c6382dbd995
serasong <gahyunsong@hufs.ac.kr>
2022-07-23 22:55:09 +0900

    --

--- summer/summerhacker/models.py
+++ summer/summerhacker/models.py
@@ -3,5 +3,19 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class User(AbstractUser):
-    pass
+
+class Member(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.CharField(max_length=255)
+    name = models.CharField(max_length=20)
+    age = models.CharField(max_length=3)
+    createdDate = models.DateTimeField(auto_now_add=True)
+    updatedDate = models.DateTimeField(auto_now=True)
+
+
+class Board(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.CharField(max_length=255)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    createDate = models.DateTimeField(auto_now_add=True)
+    updatedDate = models.DateTimeField(auto_now=True)
--- summer/summerhacker/static/js/header.js
+++ summer/summerhacker/static/js/header.js
@@ -5,7 +5,7 @@ var header = `
     <li><button class = "menu del" ><a href="bug report.html">Bug report</a></button></li>
 </ul>
 <ul class = "mainheader" id = "right">
-    <li><button class = "hvr-float-shadow menu del" ><a href="write1.html"><i class="fa-regular fa-pen-to-square"></i></a></button></li>
+    <li><button class = "hvr-float-shadow menu del" ><a href="{% url 'write1' %}"><i class="fa-regular fa-pen-to-square"></i></a></button></li>
     <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-heart"></i></a></button></li>
     <li><button class = "hvr-float-shadow menu" id="us"><a href="#"><i class="fa-regular fa-user"></i></a></button></li>
 </ul>
@@ -22,5 +22,5 @@ user.addEventListener('click', () => {
 })
 
 ham.addEventListener('click', () => {
-    document.getElementById('sidemenu').style.width='250px';
+    document.getElementById('sidemenu').style.width = '250px';
 })
