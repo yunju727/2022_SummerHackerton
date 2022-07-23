f614813d1f14b4b5a62bf39684da85ce29eaf5f7
yunju727 <80828786+yunju727@users.noreply.github.com>
2022-07-23 17:45:09 +0900

    유저기능

--- summer/summerhacker/views.py
+++ summer/summerhacker/views.py
@@ -9,21 +9,6 @@ from django.shortcuts import redirect
 
 # @login_message_required
 # @admin_required
-def notice_write_view(request):
-    if request.method == "POST":
-        form = NoticeWriteForm(request.POST)
-        user = request.session['user_id']
-        user_id = User.objects.get(user_id=user)
-
-        if form.is_valid():
-            notice = form.save(commit=False)
-            notice.writer = user_id
-            notice.save()
-            return redirect('notice:notice_list')
-    else:
-        form = NoticeWriteForm()
-
-    return render(request, "notice/notice_write.html", {'form': form})
 
 # Create your views here.
 
