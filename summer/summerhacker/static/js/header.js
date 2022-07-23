45fa543e2c2ad7f1184ee66ea8602ef40c984424
CE-sw <2seowon@naver.com>
2022-07-23 23:42:43 +0900

    .

--- summer/summerhacker/static/js/header.js
+++ summer/summerhacker/static/js/header.js
@@ -5,7 +5,7 @@ var header = `
     <li><button class = "menu del" ><a href="bug report.html">Bug report</a></button></li>
 </ul>
 <ul class = "mainheader" id = "right">
-    <li><button class = "hvr-float-shadow menu del" ><a href="{% url 'write1' %}"><i class="fa-regular fa-pen-to-square"></i></a></button></li>
+    <li><button class = "hvr-float-shadow menu del" ><a href="write1.html"><i class="fa-regular fa-pen-to-square"></i></a></button></li>
     <li><button class = "hvr-float-shadow menu" ><a href="#"><i class="fa-regular fa-heart"></i></a></button></li>
     <li><button class = "hvr-float-shadow menu" id="us"><a href="#"><i class="fa-regular fa-user"></i></a></button></li>
 </ul>
