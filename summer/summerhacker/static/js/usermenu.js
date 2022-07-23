bfc6adaa1de1047e0e50126439f0b491a40b4e15
CE-sw <2seowon@naver.com>
2022-07-24 00:31:33 +0900

    .

--- summer/summerhacker/static/js/usermenu.js
+++ summer/summerhacker/static/js/usermenu.js
@@ -2,8 +2,13 @@ var usermenu = `
 <div class="usermenu">
 <li id="guide">Please Login</li>
 <ul id="usermenu">
-    <li><a href="user.html" id="prof">Profile</a></li>
-    <li><a href="login.html">Sign in</a></li>
+    {% if user.is_authenticated %}
+        <li><a href="user.html" id="prof">Profile</a></li>
+        <li><a href="login.html">Sign in</a></li>
+    {% else %}
+        <li><a href="user.html" id="prof">Profile</a></li>
+        <li><a href="login.html">Sign Out</a></li>
+    {% endif %}
 </ul>
 </div>`
 
--- summer/users/views.py
+++ summer/users/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        if user is not None: 
+            login(request, user)
+        else: print("실패")
     return render(request, "users/login.html")
